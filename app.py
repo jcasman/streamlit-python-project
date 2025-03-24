53210318e6691c2300b4290ab9f5959a9a511de0
Jesse Casman <jcasman@oppkey.com>
2025-03-24 09:42:02 -0700

    adding title

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import streamlit as st
+
+st.title('This is my Hellow World app!')
+
 st.write('Hello World!')
+
+st. header('This is a header with a divider below it')
+
+st.markdown("This is created using st.markdown")
+
+x = st.slider("Choose an x value", 1, 10)
+x
